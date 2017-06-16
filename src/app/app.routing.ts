c447417da9bdb7d1cfba617a8c303757f9f3adbc
Ben Petrosky <benpetrosky@hotmail.com>
2017-06-16 10:19:35 -0700

    add a hardcoded list of players through firebase utilizing a service

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,7 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent }   from './about/about.component';
+import { PlayerListComponent } from './player-list/player-list.component'
 // import { AdminComponent }   from './admin/admin.component';
 
 const appRoutes: Routes = [
@@ -8,10 +9,10 @@ const appRoutes: Routes = [
     path: 'about',
     component: AboutComponent
   },
-  // {
-  //   path: 'admin',
-  //   component: AdminComponent
-  // }
+  {
+    path: 'player-list',
+    component: PlayerListComponent
+  }
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
